0cd643bdc9e6b0ab7ebf05993f86c566b6c4ae37
rowlandjl <rowlandjl@vcu.edu>
2015-12-18 20:00:08 -0500

    first commit

---
+++ galapagos_summary.rb
@@ -0,0 +1,11 @@
+departure_date = "May 1st"
+return_date = "May 14th"
+hotel_name = "Iguana Crossing"
+num_of_iguanas = "20"
+favorite_beach = "Santa Cruz Island"
+num_of_tortoises = "6"
+
+puts "We left #{departure_date} for The Galapagos Islands and stayed" +
+" at the #{hotel_name} hotel. During the trip I saw #{num_of_iguanas}" +
+" iguanas and #{num_of_tortoises} giant tortoises. My favorite beach was" +
+" #{favorite_beach}. After an awesome trip we returned home on #{return_date}."
